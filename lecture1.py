3f55de45bfb7bd02f57ba2a819acb19db83ee757
Tahsin Mahamud <126023417+TahsinMahamud99@users.noreply.github.com>
2023-07-18 15:38:29 -0400

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,45 @@
+
+#-----------------------LECTURE 1---------------------
+
+'''TOPICS COVERED:
+1. printing
+2. assigining
+3. formatting
+4. input
+5. output
+6. (also commenting)
+'''
+
+
+#printing
+print("My name is Codoking")
+print('I am 22 years old')
+
+#assigning
+name = 'Codoking'
+age = 22
+
+print()
+
+
+print('My name is', name)
+print('I am', age, 'years old')
+
+price = 9000
+
+#formatting
+print(f'This bottle of water is over ${price}')
+print('Nappa, his power lovel, it\'s over 9000!')
+
+
+print()
+#user input
+name2 = input('Enter your name dufus!')
+age2 = input('Enter your age jerk!')
+
+print(f'My name is {name2} and I am {age2} years old')
+
+
+#CODOKING
+#@the_codoking
+
